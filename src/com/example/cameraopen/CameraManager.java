9310624aa31a2ee42d1fdba156d9adf8374a71ac
liang.wang <liang.wang@ck-telecom.com>
2014-02-25 16:26:10 +0800

    rm commits
    [Solution Description]
    modified:   src/com/example/cameraopen/CameraManager.java
    [Other info]
    N/A

--- src/com/example/cameraopen/CameraManager.java
+++ src/com/example/cameraopen/CameraManager.java
@@ -4,52 +4,16 @@ public class CameraManager {
        public static final String TAG = "CameraManager";
 
        // Values for capture mode settings.
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_NORMAL = "normal";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_BEST_SHOT = "bestshot";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_EV_BRACKET_SHOT = "evbracketshot";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_BURST_SHOT = "burstshot";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_SMILE_SHOT = "smileshot";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_PANORAMA_SHOT = "autorama";
-       /**
-         * @hide
-        */
        public static final String CAPTURE_MODE_ASD = "asd";
-       /**
-        * @hide
-        */
        public static final String CAPTURE_MODE_FB = "face_beauty";
-       /**
-        * @hide
-        */
        public static final String KEY_MAX_NUM_DETECTED_OBJECT = "max-num-ot";  
-       /**
-        * @hide
-        */     
        public static final String CAPTURE_MODE_S3D = "single3d";
-       /**
-        * @hide
-        */
        public static final String CAPTURE_MODE_PANORAMA3D = "panorama3dmode";
-       /**
-        * @hide
-        */
        public static final String CAPTURE_MODE_CONTINUOUS_SHOT = "continuousshot";
 }
