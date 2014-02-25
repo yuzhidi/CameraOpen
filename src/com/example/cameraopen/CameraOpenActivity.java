c0a3bceae1c3708f845ccf8d4fe23a984ce5fbdc
liang.wang <liang.wang@ck-telecom.com>
2014-02-25 14:23:11 +0800

    format code
    [Solution Description]
    modified:   src/com/example/cameraopen/CameraOpenActivity.java
    modified:   src/com/example/cameraopen/DetectMethods.java
    [Other info]
    N/A

--- src/com/example/cameraopen/CameraOpenActivity.java
+++ src/com/example/cameraopen/CameraOpenActivity.java
@@ -14,71 +14,71 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class CameraOpenActivity extends Activity {
-	Camera mCamera;
-	int numberOfCameras;
-	int cameraCurrentlyLocked;
+    Camera mCamera;
+    int numberOfCameras;
+    int cameraCurrentlyLocked;
 
-	// The first rear facing camera
-	int defaultCameraId;
-	TextView mTextView;
-	ListView mListView;
+    // The first rear facing camera
+    int defaultCameraId;
+    TextView mTextView;
+    ListView mListView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// Hide the window title.
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		setContentView(R.layout.main);
+        setContentView(R.layout.main);
 
-		mTextView = (TextView) findViewById(R.id.textview);
-		mListView = (ListView) findViewById(R.id.listview);
-		// Find the total number of cameras available
-		numberOfCameras = Camera.getNumberOfCameras();
-		mTextView.setText(Integer.toString(numberOfCameras));
-		// Find the ID of the default camera
-		CameraInfo cameraInfo = new CameraInfo();
-		for (int i = 0; i < numberOfCameras; i++) {
-			Camera.getCameraInfo(i, cameraInfo);
-			if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
-				defaultCameraId = i;
-			}
-		}
-	}
+        mTextView = (TextView) findViewById(R.id.textview);
+        mListView = (ListView) findViewById(R.id.listview);
+        // Find the total number of cameras available
+        numberOfCameras = Camera.getNumberOfCameras();
+        mTextView.setText(Integer.toString(numberOfCameras));
+        // Find the ID of the default camera
+        CameraInfo cameraInfo = new CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+            }
+        }
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.camera_open, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.camera_open, menu);
+        return true;
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-		// Open the default i.e. the first rear facing camera.
-		mCamera = Camera.open();
-		cameraCurrentlyLocked = defaultCameraId;
-		List<String> alist = DetectMethods
-				.detectMethods("android.hardware.Camera");
-		ArrayAdapter<String> adt = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, alist);
-		mListView.setAdapter(adt);
-		mListView.setTextFilterEnabled(true);
-	}
+        // Open the default i.e. the first rear facing camera.
+        mCamera = Camera.open();
+        cameraCurrentlyLocked = defaultCameraId;
+        List<String> alist = DetectMethods
+                .detectMethods("android.hardware.Camera");
+        ArrayAdapter<String> adt = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, alist);
+        mListView.setAdapter(adt);
+        mListView.setTextFilterEnabled(true);
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-		// Because the Camera object is a shared resource, it's very
-		// important to release it when the activity is paused.
-		if (mCamera != null) {
-			mCamera.release();
-			mCamera = null;
-		}
-	}
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 }
--- src/com/example/cameraopen/DetectMethods.java
+++ src/com/example/cameraopen/DetectMethods.java
@@ -10,52 +10,52 @@ import java.util.regex.Pattern;
 import android.util.Log;
 
 public class DetectMethods {
-	public static final String TAG = "ShowMethods";
-	private static Pattern p = Pattern.compile("\\w+\\.");
+    public static final String TAG = "ShowMethods";
+    private static Pattern p = Pattern.compile("\\w+\\.");
 
-	public static List<String> detectMethods(String args) {
-		ArrayList<String> alist = new ArrayList<String>();
-		String s = null;
-		try {
-			Class<?> c = Class.forName(args);
-			Method[] methods = c.getMethods();
-			Log.v(TAG, "======Methods=========");
-			for (Method method : methods) {
-				s = p.matcher(method.toString()).replaceAll("");
-				Log.v(TAG, s);
-				alist.add(s);
-			}
-			Log.v(TAG, "======Fields=========");
-			Field[] fields = c.getFields();
-			for (Field field : fields) {
-				s = p.matcher(field.toString()).replaceAll("");
-				Log.v(TAG, s);
-				alist.add(s);
-			}
-			Log.v(TAG, "=========Annotations================");
-			Annotation[] annotations = c.getAnnotations();
-			for (Annotation annotation : annotations) {
-				s = p.matcher(annotation.toString()).replaceAll("");
-				Log.v(TAG, s);
-				alist.add(s);
-			}
-			Log.v(TAG, "========= getClasses================");
-			Class[] classes = c.getClasses();
-			for (Class classe : classes) {
-				s = p.matcher(classe.toString()).replaceAll("");
-				Log.v(TAG, s);
-				alist.add(s);
-			}
-			Log.v(TAG, "=========getDeclaredClasses================");
-			Class[] declaredClasses = c.getDeclaredClasses();
-			for (Class classe : declaredClasses) {
-				s = p.matcher(classe.toString()).replaceAll("");
-				Log.v(TAG, s);
-				alist.add(s);
-			}
-		} catch (ClassNotFoundException e) {
-			Log.v(TAG, "No such class: " + e);
-		}
-		return alist;
-	}
+    public static List<String> detectMethods(String args) {
+        ArrayList<String> alist = new ArrayList<String>();
+        String s = null;
+        try {
+            Class<?> c = Class.forName(args);
+            Method[] methods = c.getMethods();
+            Log.v(TAG, "======Methods=========");
+            for (Method method : methods) {
+                s = p.matcher(method.toString()).replaceAll("");
+                Log.v(TAG, s);
+                alist.add(s);
+            }
+            Log.v(TAG, "======Fields=========");
+            Field[] fields = c.getFields();
+            for (Field field : fields) {
+                s = p.matcher(field.toString()).replaceAll("");
+                Log.v(TAG, s);
+                alist.add(s);
+            }
+            Log.v(TAG, "=========Annotations================");
+            Annotation[] annotations = c.getAnnotations();
+            for (Annotation annotation : annotations) {
+                s = p.matcher(annotation.toString()).replaceAll("");
+                Log.v(TAG, s);
+                alist.add(s);
+            }
+            Log.v(TAG, "========= getClasses================");
+            Class[] classes = c.getClasses();
+            for (Class classe : classes) {
+                s = p.matcher(classe.toString()).replaceAll("");
+                Log.v(TAG, s);
+                alist.add(s);
+            }
+            Log.v(TAG, "=========getDeclaredClasses================");
+            Class[] declaredClasses = c.getDeclaredClasses();
+            for (Class classe : declaredClasses) {
+                s = p.matcher(classe.toString()).replaceAll("");
+                Log.v(TAG, s);
+                alist.add(s);
+            }
+        } catch (ClassNotFoundException e) {
+            Log.v(TAG, "No such class: " + e);
+        }
+        return alist;
+    }
 }
