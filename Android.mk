8412782797e696ecb61b48756269306fdbd42f8d
liang.wang <liang.wang@ck-telecom.com>
2014-03-12 16:10:47 +0800

    [][][][add workspace resource]
    [Solution Description]
    new file:   .classpath
    new file:   .settings/org.eclipse.jdt.core.prefs
    new file:   Android.mk
    new file:   project.properties
    [Other info]
    N/A

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH2 := $(LOCAL_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res 
+
+LOCAL_PACKAGE_NAME := CameraOpen
+
+
+include $(BUILD_PACKAGE)
